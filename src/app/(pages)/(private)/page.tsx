e007babd8c38863719cd3ed5abb603f875e94fd2
roLLlyTHuK <ircvipua@gmail.com>
2024-12-23 13:06:08 +0200

    change private

--- src/app/(pages)/(private)/page.tsx
+++ src/app/(pages)/(private)/page.tsx
@@ -4,12 +4,9 @@ export const metadata: Metadata = {
   title: "Прайс",
   description: "Сторінка прайсу користувача",
 };
-export default function Prices() {
+export default function Private() {
   return (
-    <main className="bg-gray-0 pt-20 pb-20">
-      <div className='w-[822px] ml-auto mr-auto container bg-white p-12 rounded-3xl shadow-pricesPage'>
-
-      </div>
-    </main>
+    <>
+    </>
   )
 }
