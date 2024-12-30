64f2c568bfadf4002d613a0f88b8de76925e8570
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-12-30 14:40:54 +0200

    start toggle

--- src/app/components/Project/Estimate/EstimateToggle.tsx
+++ src/app/components/Project/Estimate/EstimateToggle.tsx
@@ -1,4 +1,9 @@
+'use client'
+import { useEffect, useState } from "react";
 import EstimateItem from "./Estimate";
+import { useUser } from "@/app/context/UserContext";
+import { ProjectItem } from "@/app/interfaces/projects";
+import { getProject } from "@/app/utils/projects";
 
  
  interface EstimateToggleProps {
@@ -6,13 +11,65 @@ import EstimateItem from "./Estimate";
 }
 
 const EstimateToggle: React.FC<EstimateToggleProps> = ({ projectId }) => {
+    const { user } = useUser();
+    const [project, setProject] = useState<ProjectItem | null>(null);
+    const [sizeEstimate, setSizeEstimate] = useState(true);
+   
+    
+      useEffect(() => {
+        getEstimate()
+      }, []);
+      
+        async function getEstimate() {
+        if (projectId !== null) {  
+            const estimate = await getProject(projectId);
+            if (estimate) {
+            setProject(estimate); 
+            }
+        }
+    }
+
+    const isAllow = user?.projectIds.filter(({ id }) => id === projectId);
+        
+   
+
+    // console.log(isAllow);
+
+    
+
     return (
         <div>
-           <div className="flex items-center justify-end gap-6 mt-6 mb-6">
+            {user?._id === project?.owner && (
+            <div className="flex items-center justify-end gap-6 mt-6 mb-6">
                 <div> <button className="block font-medium text-sm px-3 py-1 text-blue-25" >Основний</button><div className="w-full h-[1px] bg-blue-25"></div></div>
                 <div><button className="block font-medium text-sm px-3 py-1 text-blue-25" >Знижений</button><div className="w-full h-[1px] bg-blue-25"></div></div>
-            </div>
-            <EstimateItem projectId={projectId} />
+            </div>    
+            )}
+
+            {isAllow && isAllow[0]?.lookAt === 'all' && (
+              <div className="flex items-center justify-end gap-6 mt-6 mb-6">
+                <div> <button className="block font-medium text-sm px-3 py-1 text-blue-25" >Основний</button><div className="w-full h-[1px] bg-blue-25"></div></div>
+                <div><button className="block font-medium text-sm px-3 py-1 text-blue-25" >Знижений</button><div className="w-full h-[1px] bg-blue-25"></div></div>
+            </div>    
+            )}
+
+           
+            {user?._id === project?.owner && (
+                <div> {sizeEstimate ? (<EstimateItem projectId={projectId} />) : (<div>SMALL</div>)}</div>
+            )}
+            
+            {isAllow && isAllow[0]?.lookAt === 'all' && (
+                 <div> {sizeEstimate ? (<EstimateItem projectId={projectId} />) : (<div>SMALL</div>)}</div>
+            )}
+
+            {isAllow && isAllow[0]?.lookAt === 'small' && (
+                <div>Small</div>
+            )}
+           
+             {isAllow && isAllow[0]?.lookAt === 'large' && (
+                <div>Large</div>
+            )}
+
         </div>
     )
  }
