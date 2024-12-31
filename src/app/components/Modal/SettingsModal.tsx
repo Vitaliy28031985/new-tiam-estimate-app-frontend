9854ba2664293979af5fe3a80a2461ee31ec7b59
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-12-31 14:46:30 +0200

    create toggle settings

--- src/app/components/Modal/SettingsModal.tsx
+++ src/app/components/Modal/SettingsModal.tsx
@@ -1,4 +1,6 @@
+import ChangeSettingsProject from "@/app/UI/ChangeSettingsProject";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 interface SettingsProps {
     id?: string | undefined;
@@ -6,11 +8,50 @@ interface SettingsProps {
     isShow?: () => void;
 }
 
-const SettingsModal: React.FC<SettingsProps> = ({ toggle, isShow, id }) =>{
+const SettingsModal: React.FC<SettingsProps> = ({ toggle, isShow, id }) => {
+    const [page, setPage] = useState('add-allow');
+    const [data, setData] = useState('add-allow');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+       const { name, value, } = e.currentTarget;
+    switch (name) {
+      case 'settings':
+            if (value === 'add-allow') {
+                setData(value);
+                setPage(value);
+        
+            }
+            if (value === 'update-allow') {
+               setData(value);
+                setPage(value); 
+            }
+             if (value === 'delete-allow') {
+               setData(value);
+                setPage(value); 
+            }
+             if (value === 'discount') {
+               setData(value);
+                setPage(value); 
+            }
+              if (value === 'low-estimate') {
+               setData(value);
+                setPage(value); 
+            }
+        break;
+
+      default:
+        return;
+    }
+
+  }
+
+    console.log(page);
+
     return (
          <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center z-50"> 
-            <section className="relative bg-white px-[71px] p-8 rounded-[24px] w-[608px] shadow-lg">
-                <button type="button" onClick={toggle} className='absolute top-3 right-3'><XMarkIcon className='size-6 text-black'/></button>
+            <section className="relative bg-white px-[20px] p-8 rounded-[24px] w-[908px] shadow-lg">  
+              <button type="button" onClick={toggle} className='absolute top-3 right-3'><XMarkIcon className='size-6 text-black' /></button>
+                <ChangeSettingsProject data={data} changeCheckbox={handleChange} />
             </section>
         </div>
     )
